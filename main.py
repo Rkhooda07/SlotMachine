c50ad2668ae6a12c0b8a33d4a5e48b04d1a7183e
Rakshit Hooda <rakshit.official703@gmail.com>
2026-01-23 01:17:09 +0530

    chore: initialise project and define game constants

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+  "🍒": 3,
+  "💸": 4,
+  " 7": 5,
+  "🎱": 6
+}
+
+symbol_value = {
+  "🍒": 5,
+  "💸": 4,
+  " 7": 3,
+  "🎱": 2
+}
