6660d7876117ee6c812e6a275f3c1a48eed22c0b
Rakshit Hooda <rakshit.official703@gmail.com>
2026-01-23 22:16:34 +0530

    feat: add win detection system and payout calculation

--- main.py
+++ main.py
@@ -49,3 +49,18 @@ def print_spin(columns):
       else:
         print(column[row], end="")
     print()
+
+def check_win(columns, lines, bet , values):
+  winnings = 0
+  winning_lines = []
+  for line in range(lines):
+    symbol = columns[0][line]
+    for column in columns:
+      symbol_to_check = column[line]
+      if symbol != symbol_to_check:
+        break
+    else:
+      winnings += values[symbol] * bet
+      winning_lines.append(line + 1)
+
+  return winnings, winning_lines
