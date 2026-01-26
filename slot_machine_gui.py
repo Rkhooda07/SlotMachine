6e6d0f0803009463de7e17c15ee681ee8858afd9
Rakshit Hooda <rakshit.official703@gmail.com>
2026-01-26 22:29:15 +0530

    chore: initialize PyQt5 app and define game constants

---
+++ slot_machine_gui.py
@@ -0,0 +1,28 @@
+import sys
+import random
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout, 
+                           QHBoxLayout, QPushButton, QLabel, QSpinBox, 
+                           QMessageBox, QGridLayout, QInputDialog)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+
+# Constants from original game
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "🍒": 3,
+    "💸": 4,
+    " 7": 5,
+    "🎱": 6
+}
+
+symbol_value = {
+    "🍒": 5,
+    "💸": 4,
+    " 7": 3,
+    "🎱": 2
+}
